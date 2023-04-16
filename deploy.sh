62cf1e012bc3de5610057ded3dee9c7d84fc38e3
arigato-mba <teeruch.k@gmail.com>
2023-04-16 15:57:34 +0700

    275. Updating the Deployment Script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker biuld -t arigatopix/multi-client -f ./client/Dockerfile ./client
-docker biuld -t arigatopix/multi-server -f ./server/Dockerfile ./server
-docker biuld -t arigatopix/multi-worker -f ./worker/Dockerfile ./worker
-docker push arigatopix/multi-client
-docker push arigatopix/multi-server
-docker push arigatopix/multi-worker
+docker biuld -t arigatopix/multi-client:latest -t arigatopix/multi-client:$SHA -f ./client/Dockerfile ./client
+docker biuld -t arigatopix/multi-server:latest -t arigatopix/multi-server:$SHA -f ./server/Dockerfile ./server
+docker biuld -t arigatopix/multi-worker:latest -t arigatopix/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push arigatopix/multi-client:latest
+docker push arigatopix/multi-server:latest
+docker push arigatopix/multi-worker:latest
+
+docker push arigatopix/multi-client:$SHA
+docker push arigatopix/multi-server:$SHA
+docker push arigatopix/multi-worker:$SHA
+
 kubctl apply -f k8s
-kubctl set image deployments/server-deployment server=arigatpix/multi-server
+kubctl set image deployments/server-deployment server=arigatpix/multi-server:$SHA
+kubctl set image deployments/client-deployment client=arigatpix/multi-client:$SHA
+kubctl set image deployments/worker-deployment worker=arigatpix/multi-worker:$SHA
